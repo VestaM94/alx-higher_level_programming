c180ad5e3fa40a10d8fc1f8343371ca44bd5bee4
VestaM94 <manganye.so@gmail.com>
2023-06-06 13:17:43 -0700

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
 for i in range(97, 123):
-    if chr(i) not in ('q', 'e'):
-        print(chr(i), end='')
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
